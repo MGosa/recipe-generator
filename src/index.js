88186b06cda7b38992958b4632de137a4dbbe9c2
MGosa <149839103+MGosa@users.noreply.github.com>
2023-12-03 22:23:49 +0000

    updated context for AI

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function recipeGenerator(event) {
 
   let apiKey = `off12bea02cd1ft1f4f0c632ee3ca6b4`;
   let prompt = `generate a short and quick recipe, the recipe have to include ${instructionElement.value}`;
-  let context = ` You are experienced home cook who specializes in quick and simple meals. Generate a recipe that has no more than 10 ingredients and will take no longer than 40 min to prepare. Display the recipre in HTML format. `;
+  let context = ` You are experienced home cook who specializes in quick and simple meals. Generate a recipe that has no more than 10 ingredients and will take no longer than 40 min to prepare. Display the recipre in HTML format.Do not include images `;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayRecipe);
